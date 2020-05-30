fa9bd48b02c17a52110f16f49b6cdc84f43552ec
iskona <isony.d@gmail.com>
2020-05-30 16:06:56 -0700

    Updates

---
+++ precache-manifest.10fcad76547c728a5917dc464a2417c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9d3e7efd5d4029145e995ac696943b",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "06f2f57f8f7baf243264",
+    "url": "/React-Employee-Directory/static/css/main.9e5e038a.chunk.css"
+  },
+  {
+    "revision": "beb4dfc4668baf15be3f",
+    "url": "/React-Employee-Directory/static/js/2.da35cbc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.da35cbc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f2f57f8f7baf243264",
+    "url": "/React-Employee-Directory/static/js/main.26790521.chunk.js"
+  },
+  {
+    "revision": "8d7462adf2df704c45c7",
+    "url": "/React-Employee-Directory/static/js/runtime-main.529585d6.js"
+  }
+]);
